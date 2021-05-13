27408bb9f442e7779d17b4877a9220bff9a6129b
Mateusz Choma <kontakt@coderoad.pl>
2021-05-13 09:04:21 +0200

    What JSX can render

--- src/index.js
+++ src/index.js
@@ -3,9 +3,72 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import reportWebVitals from './reportWebVitals'
 
+const string1 = 'Ala ma'
+const string2 = 'kota'
+
 ReactDOM.render(
   <div>
     <h1>What JSX can render</h1>
+    <h4>String</h4>
+    <div>
+      {`${string1} ${string2}`}
+    </div>
+    <h4>Number</h4>
+    <div>
+      {2 + 2}
+    </div>
+    <div>
+      {1.124}
+    </div>
+    <h4>Boolean</h4>
+    <div>
+      {String(true)}:{' '}
+      {true}{' '}
+      |
+    </div>
+    <div>
+      {String(false)}:{' '}
+      {false}{' '}
+      |
+    </div>
+    <h4>undefined</h4>
+    <div>
+      {undefined}
+    </div>
+    <h4>null</h4>
+    <div>
+      {null}
+    </div>
+    <h4>Array</h4>
+    <div>
+      {['Ala ', 'Ola ', 'Ela']}
+    </div>
+    <div>
+      {[1, 2, 3, 4]}
+    </div>
+    <h4>Functions</h4>
+    <div>
+      {(() => { console.log('Im an function!') })}
+    </div>
+    <div>
+      {(() => { console.log('Im an function!') })()}
+    </div>
+    <div>
+      {(() => { return 'Some string value' })()}
+    </div>
+    <h4>Object</h4>
+    {/* <div>
+      {{
+        name: 'Mateusz',
+        company: 'CodeRoad'
+      }}
+    </div> */}
+    <div>
+      {Object.entries({
+        name: 'Mateusz',
+        company: 'CodeRoad'
+      })}
+    </div>
   </div>,
   document.getElementById('root')
 )
