5a68c690187c1ec28af55f7a796e2520dfd09b7c
Mateusz Choma <kontakt@coderoad.pl>
2021-05-18 07:11:57 +0200

    Multiple ways to render conditionally

--- src/index.js
+++ src/index.js
@@ -3,9 +3,45 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import reportWebVitals from './reportWebVitals'
 
+const stringA = 'Ala ma kota'
+const stringB = 'Kot ma Ale'
+
+const condition = true
+
+let result = ''
+if (condition) {
+  result = stringA
+} else {
+  result = stringB
+}
+
+const makeResult = () => {
+  if (condition) {
+    return stringA
+  } else {
+    return stringB
+  }
+}
+
 ReactDOM.render(
   <div>
-
+    {
+      condition ? stringA : stringB
+    }
+    <br />
+    {result}
+    <br />
+    {makeResult()}
+    <br />
+    {
+      (() => {
+        if (condition) {
+          return stringA
+        } else {
+          return stringB
+        }
+      })()
+    }
   </div>,
   document.getElementById('root')
 )
