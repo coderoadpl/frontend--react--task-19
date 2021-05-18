6b72bd17fd11d2151755078df76f1b448f1b30f9
Mateusz Choma <kontakt@coderoad.pl>
2021-05-18 06:27:15 +0200

    Rendering array with JSX elements

--- src/index.js
+++ src/index.js
@@ -1,74 +1,25 @@
+/* eslint-disable react/jsx-key */
+
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import reportWebVitals from './reportWebVitals'
 
-const string1 = 'Ala ma'
-const string2 = 'kota'
+// eslint-disable-next-line no-unused-vars
+const names = ['Ala', 'Ola', 'Ela']
+const namesElements = [
+  <li>Ala</li>,
+  <li>Ola</li>,
+  <li>Ela</li>
+]
+
+window.namesElements = namesElements
 
 ReactDOM.render(
   <div>
-    <h1>What JSX can render</h1>
-    <h4>String</h4>
-    <div>
-      {`${string1} ${string2}`}
-    </div>
-    <h4>Number</h4>
-    <div>
-      {2 + 2}
-    </div>
-    <div>
-      {1.124}
-    </div>
-    <h4>Boolean</h4>
-    <div>
-      {String(true)}:{' '}
-      {true}{' '}
-      |
-    </div>
-    <div>
-      {String(false)}:{' '}
-      {false}{' '}
-      |
-    </div>
-    <h4>undefined</h4>
-    <div>
-      {undefined}
-    </div>
-    <h4>null</h4>
-    <div>
-      {null}
-    </div>
-    <h4>Array</h4>
-    <div>
-      {['Ala ', 'Ola ', 'Ela']}
-    </div>
-    <div>
-      {[1, 2, 3, 4]}
-    </div>
-    <h4>Functions</h4>
-    <div>
-      {(() => { console.log('Im an function!') })}
-    </div>
-    <div>
-      {(() => { console.log('Im an function!') })()}
-    </div>
-    <div>
-      {(() => { return 'Some string value' })()}
-    </div>
-    <h4>Object</h4>
-    {/* <div>
-      {{
-        name: 'Mateusz',
-        company: 'CodeRoad'
-      }}
-    </div> */}
-    <div>
-      {Object.entries({
-        name: 'Mateusz',
-        company: 'CodeRoad'
-      })}
-    </div>
+    <ul>
+      {namesElements}
+    </ul>
   </div>,
   document.getElementById('root')
 )
