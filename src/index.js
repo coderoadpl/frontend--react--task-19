c854d94b6a3da4ce993a16abaa34660005c46230
Mateusz Choma <kontakt@coderoad.pl>
2021-05-18 07:01:40 +0200

    Cleared

--- src/index.js
+++ src/index.js
@@ -3,59 +3,9 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import reportWebVitals from './reportWebVitals'
 
-const files = [
-  {
-    id: 1,
-    name: 'File 1'
-  },
-  {
-    id: 2,
-    name: 'Folder 1',
-    files: [
-      {
-        id: 2,
-        name: 'File 2'
-      },
-      {
-        id: 3,
-        name: 'File 3'
-      }
-    ]
-  },
-  {
-    id: 4,
-    name: 'File 4'
-  },
-  {
-    id: 5,
-    name: 'File 5'
-  }
-]
-
 ReactDOM.render(
   <div>
-    <ul>
-      {
-        files.map((file) => {
-          return (
-            <li key={file.id}>
-              {file.name}
-              <ul>
-                {
-                  file.files && file.files.map((innerFile) => {
-                    return (
-                      <li key={innerFile.id}>
-                        {innerFile.name}
-                      </li>
-                    )
-                  })
-                }
-              </ul>
-            </li>
-          )
-        })
-      }
-    </ul>
+
   </div>,
   document.getElementById('root')
 )
