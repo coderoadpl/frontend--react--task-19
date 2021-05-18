8a70a0c3361aee37177fd7fea1078f67d30cac8f
Mateusz Choma <kontakt@coderoad.pl>
2021-05-18 07:29:10 +0200

    Using filter with map

--- src/index.js
+++ src/index.js
@@ -3,8 +3,44 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import reportWebVitals from './reportWebVitals'
 
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
 ReactDOM.render(
   <div>
+    <h4>All numbers</h4>
+    <ul>
+      {
+        numbers.map((number, i) => {
+          return (
+            <li key={i}>{number}</li>
+          )
+        })
+      }
+    </ul>
+    <h4>Even numbers</h4>
+    <ul>
+      {
+        numbers
+          .filter((number) => number % 2 === 0)
+          .map((number, i) => {
+            return (
+              <li key={i}>{number}</li>
+            )
+          })
+      }
+    </ul>
+    <h4>Odd numbers</h4>
+    <ul>
+      {
+        numbers
+          .filter((number) => number % 2 !== 0)
+          .map((number, i) => {
+            return (
+              <li key={i}>{number}</li>
+            )
+          })
+      }
+    </ul>
   </div>,
   document.getElementById('root')
 )
