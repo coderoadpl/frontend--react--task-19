6614748b4ea9bc333ad5b68040e95a64d60d3fe2
Mateusz Choma <kontakt@coderoad.pl>
2021-05-24 08:37:32 +0200

    Cleared

--- src/index.js
+++ src/index.js
@@ -1,37 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import HelloJS from './HelloJS'
 import Hello from './Hello'
 
 import './index.css'
 import reportWebVitals from './reportWebVitals'
 
-const helloJS1 = new HelloJS()
-const helloJS2 = new HelloJS()
-document.body.appendChild(helloJS1.render())
-document.body.appendChild(helloJS2.render())
-
-/*
-
-Code below in ReactDOM.render is the same as in JSX:
-
-<div>
-  <Hello />
-  <Hello />
-</div>
-
-*/
-
 ReactDOM.render(
-  React.createElement(
-    'div',
-    {},
-    [
-      React.createElement(Hello),
-      React.createElement(Hello)
-    ]
-  ),
+  <div>
+    <Hello />
+    <Hello />
+  </div>,
   document.getElementById('root')
 )
 
