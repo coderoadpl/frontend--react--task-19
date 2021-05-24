069e96b76cea9d56e725300541f1bfa2f1bc7b5a
Mateusz Choma <kontakt@coderoad.pl>
2021-05-24 08:59:24 +0200

    CounterJS without re-render

--- src/CounterJS.js
+++ src/CounterJS.js
@@ -1,8 +1,28 @@
 export class CounterJS {
+  constructor () {
+    this.number = 0
+  }
+
   render () {
+    const container = document.createElement('div')
+
     const h1 = document.createElement('h1')
-    h1.innerText = 'Hello CodeRoad (pure JS)'
-    return h1
+    h1.innerText = this.number
+
+    const button = document.createElement('button')
+    button.innerText = '+'
+
+    button.addEventListener(
+      'click',
+      () => {
+        this.number = this.number + 1
+      }
+    )
+
+    container.appendChild(h1)
+    container.appendChild(button)
+
+    return container
   }
 }
 
