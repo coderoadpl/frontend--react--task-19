fff7927923ccb19319d34b14ce3f9bd4d970e44c
Mateusz Choma <kontakt@coderoad.pl>
2021-05-24 07:41:27 +0200

    Cleared

--- src/index.js
+++ src/index.js
@@ -3,32 +3,8 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import reportWebVitals from './reportWebVitals'
 
-const isEven = (number) => number % 2 === 0
-const isOdd = (number) => number % 2 !== 0
-const renderNumber = (number, i) => {
-  return (
-    <li key={i}>{number}</li>
-  )
-}
-
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-const evenNumbers = numbers.filter(isEven)
-const oddNumbers = numbers.filter(isOdd)
-
 ReactDOM.render(
   <div>
-    <h4>All numbers</h4>
-    <ul>
-      {numbers.map(renderNumber)}
-    </ul>
-    <h4>Even numbers</h4>
-    <ul>
-      {evenNumbers.map(renderNumber)}
-    </ul>
-    <h4>Odd numbers</h4>
-    <ul>
-      {oddNumbers.map(renderNumber)}
-    </ul>
   </div>,
   document.getElementById('root')
 )
